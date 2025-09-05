bd4fee5e49b6af9b95014811ef744e40727dc670
Kailash Joshi <kailash@example.com>
2025-09-05 14:45:14 +0545

    created new action method

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) :base(options) 
+        { 
+
+         }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "kailash", DisplayOrder = 0 },
+                new Category { Id = 2, Name = "joshi", DisplayOrder = 1 },
+                new Category { Id = 3, Name = "dhangadhi", DisplayOrder = 2 }
+                );
+        }
+    }
+}
